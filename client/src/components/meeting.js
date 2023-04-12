0999bd725ecec11aaefdd596e7025240c6fb75fd
Benjamin Phillips <benjamingt537@gmail.com>
2023-04-12 00:30:53 -0400

    get meetings working with the voiceSDK api

--- client/src/components/meeting.js
+++ client/src/components/meeting.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
-  MeetingProvider,
-  MeetingConsumer,
-  useMeeting,
-  useParticipant,
+    MeetingProvider,
+    MeetingConsumer,
+    useMeeting,
+    useParticipant,
 } from "@videosdk.live/react-sdk";
 import { authToken, createMeeting } from "../utils/VideoSDK";
 import ReactPlayer from "react-player";
 
+
 function JoinScreen({ getMeetingAndToken }) {
     const [meetingId, setMeetingId] = useState(null);
     const onClick = async () => {
-      await getMeetingAndToken(meetingId);
+        await getMeetingAndToken(meetingId);
     };
     return (
         <div>
@@ -24,44 +25,138 @@ function JoinScreen({ getMeetingAndToken }) {
 }
 
 function ParticipantView(props) {
-  return null;
+    const micRef = useRef(null);
+    const { webcamStream, micStream, webcamOn, micOn, isLocal, displayName } =
+        useParticipant(props.participantId);
+
+    const videoStream = useMemo(() => {
+        if (webcamOn && webcamStream) {
+            const mediaStream = new MediaStream();
+            mediaStream.addTrack(webcamStream.track);
+            return mediaStream;
+        }
+    }, [webcamStream, webcamOn]);
+
+    useEffect(() => {
+        if (micRef.current) {
+            if (micOn && micStream) {
+                const mediaStream = new MediaStream();
+                mediaStream.addTrack(micStream.track);
+
+                micRef.current.srcObject = mediaStream;
+                micRef.current
+                    .play()
+                    .catch((error) =>
+                        console.error("videoElem.current.play() failed", error)
+                    );
+            } else {
+                micRef.current.srcObject = null;
+            }
+        }
+    }, [micStream, micOn]);
+
+    return (
+        <div>
+            <p>
+                Participant: {displayName} | Webcam: {webcamOn ? "ON" : "OFF"} | Mic:{" "}
+                {micOn ? "ON" : "OFF"}
+            </p>
+            <audio ref={micRef} autoPlay playsInline muted={isLocal} />
+            {webcamOn && (
+                <ReactPlayer
+                    playsinline
+                    pip={false}
+                    light={false}
+                    controls={false}
+                    muted={true}
+                    playing={true}
+                    //
+                    url={videoStream}
+                    //
+                    height={"300px"}
+                    width={"300px"}
+                    onError={(err) => {
+                        console.log(err, "participant video error");
+                    }}
+                />
+            )}
+        </div>
+    );
 }
 
-function Controls(props) {
-  return null;
+function Controls() {
+    const { leave, toggleMic, toggleWebcam } = useMeeting();
+    return (
+        <div>
+            <button onClick={() => leave()}>Leave</button>
+            <button onClick={() => toggleMic()}>toggleMic</button>
+            <button onClick={() => toggleWebcam()}>toggleWebcam</button>
+        </div>
+    );
 }
 
 function MeetingView(props) {
-  return null;
+    const [joined, setJoined] = useState(null);
+    const { join, participants } = useMeeting({
+        onMeetingJoined: () => {
+            setJoined("JOINED");
+        },
+        onMeetingLeft: () => {
+            props.onMeetingLeave();
+        },
+    });
+    const joinMeeting = () => {
+        setJoined("JOINING");
+        join();
+    };
+
+    return (
+        <div className="container">
+            <h3>Meeting Id: {props.meetingId}</h3>
+            {joined && joined == "JOINED" ? (
+                <div>
+                    <Controls />
+                    {[...participants.keys()].map((participantId) => (
+                        <ParticipantView
+                            participantId={participantId}
+                            key={participantId}
+                        />
+                    ))}
+                </div>
+            ) : joined && joined == "JOINING" ? (
+                <p>Joining the meeting...</p>
+            ) : (
+                <button onClick={joinMeeting}>Join</button>
+            )}
+        </div>
+    );
 }
 
 export default function Meeting() {
-  const [meetingId, setMeetingId] = useState(null);
+    const [meetingId, setMeetingId] = useState(null);
 
-  //Getting the meeting id by calling the api we just wrote
-  const getMeetingAndToken = async (id) => {
-    const meetingId = id == null ? await createMeeting({ token: authToken }) : id;
-    setMeetingId(meetingId);
-  };
+    const getMeetingAndToken = async (id) => {
+        const meetingId = id == null ? await createMeeting({ token: authToken }) : id;
+        setMeetingId(meetingId);
+    };
 
-  //This will set Meeting Id to null when meeting is left or ended
-  const onMeetingLeave = () => {
-    setMeetingId(null);
-  };
+    const onMeetingLeave = () => {
+        setMeetingId(null);
+    };
 
-  return authToken && meetingId ? (
-    <MeetingProvider
-      config={{
-        meetingId,
-        micEnabled: true,
-        webcamEnabled: true,
-        name: "C.V. Raman",
-      }}
-      token={authToken}
-    >
-      <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
-    </MeetingProvider>
-  ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
-  );
+    return authToken && meetingId ? (
+        <MeetingProvider
+            config={{
+                meetingId,
+                micEnabled: true,
+                webcamEnabled: true,
+                name: "C.V. Raman",
+            }}
+            token={authToken}
+        >
+            <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
+        </MeetingProvider>
+    ) : (
+        <JoinScreen getMeetingAndToken={getMeetingAndToken} />
+    );
 }
--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: `/api`,
+    baseURL: process.env.BASE_URL || `/api`,
     withCredentials: true
 })
